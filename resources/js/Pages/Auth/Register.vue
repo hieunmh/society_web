894db723ab205ec1ea3c51f6cc0ebde3bcd33a30
hr753 <minhieu433@gmail.com>
2022-11-12 13:31:03 +0700

    add database and fix register

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -6,6 +6,10 @@ import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
 
+defineProps({
+    errors: Object
+})
+
 const form = useForm({
     name: '',
     username: '',
@@ -30,31 +34,31 @@ const submit = () => {
             <div>
                 <InputLabel for="name" value="Name" />
                 <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name"  autofocus autocomplete="name" />
-                <InputError class="mt-2" :message="form.errors.name" />
+                <InputError class="mt-2" :message="errors.name" />
             </div>
 
             <div class="mt-4">
                 <InputLabel for="username" value="Username" />
                 <TextInput id="username" type="text" class="mt-1 block w-full" v-model="form.username"  autocomplete="username" />
-                <InputError class="mt-2" :message="form.errors.username" />
+                <InputError class="mt-2" :message="errors.username" />
             </div>
 
             <div class="mt-4">
                 <InputLabel for="email" value="Email" />
                 <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email"  autocomplete="email" />
-                <InputError class="mt-2" :message="form.errors.email" />
+                <InputError class="mt-2" :message="errors.email" />
             </div>
 
             <div class="mt-4">
                 <InputLabel for="password" value="Password" />
                 <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password"  autocomplete="new-password" />
-                <InputError class="mt-2" :message="form.errors.password" />
+                <InputError class="mt-2" :message="errors.password" />
             </div>
 
             <div class="mt-4">
                 <InputLabel for="password_confirmation" value="Confirm Password" />
                 <TextInput id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation"  autocomplete="new-password" />
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                <InputError class="mt-2" :message="errors.password_confirmation" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
