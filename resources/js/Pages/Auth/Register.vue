a063ec0fcb34b612005389d4592fe299272c7496
hr753 <minhieu433@gmail.com>
2022-11-12 14:38:04 +0700

    add community

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -58,7 +58,7 @@ const submit = () => {
             <div class="mt-4">
                 <InputLabel for="password_confirmation" value="Confirm Password" />
                 <TextInput id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation"  autocomplete="new-password" />
-                <InputError class="mt-2" :message="errors.password_confirmation" />
+                <InputError class="mt-2" :message=" errors.password_confirmation" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
